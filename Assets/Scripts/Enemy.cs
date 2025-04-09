ba3bf4f21d3646a0529019adcae6d203e5f31dab
lauthekiller <39975051+lauthekiller@users.noreply.github.com>
2025-04-08 23:44:15 -0500

    enemy 3

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gruentscrip : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject BalaPrefab;
+    private float LastShoot;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = Player.transform.position - transform.position;
+
+
+        if (direction.x >= 0.0f)
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        else
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+
+        float distance = Mathf.Abs(Player.transform.position.x - transform.position.x);
+
+        if (distance < 1.0f && Time.time > LastShoot + 1.0f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
+    }
+    private void Shoot()
+    {
+
+    }
+
+}
